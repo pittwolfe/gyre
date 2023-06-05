8e1125a08cef1092f6aa9cac6a01a3737e926cf1
Christopher L. Pitt Wolfe <christopher.wolfe@stonybrook.edu>
2023-06-05 14:55:40 -0400

    Updates!

---
+++ slope/3layer/08/001b/batch.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+#SBATCH --job-name=gyre_001
+#SBATCH --output=output_%j.txt
+#SBATCH --ntasks-per-node=28
+#SBATCH --nodes=4
+#SBATCH --time=6:00:00
+#SBATCH -p long-28core
+##SBATCH --nodes=8
+##SBATCH --time=0:30:00
+##SBATCH -p debug-28core
+#SBATCH --mail-type=BEGIN,END
+#SBATCH --mail-user=christopher.wolfe@stonybrook.edu
+
+module load intel/compiler/64/2020/20.0.2
+module load intel/mkl/64/2020/20.0.2
+module load intel/mpi/64/2020/20.0.2
+module load netcdf-fortran
+
+cd $SLURM_SUBMIT_DIR
+mkdir -p OUTPUT
+mkdir -p RESTART
+mkdir -p DIAG
+
+date
+mpirun ./MOM6
+date
